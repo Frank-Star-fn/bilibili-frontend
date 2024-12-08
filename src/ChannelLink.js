54d21a313ddf053ba8c718eb1976e5c9550e673d
Frank-Star-fn <2439206073@qq.com>
2024-12-08 11:09:01 +0800

    upd

--- src/ChannelLink.js
+++ src/ChannelLink.js
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
 
 function ChannelLink(props) {
+  const local_website = "http://localhost:3000/";
 
   return (
     <Fragment>
-      <a href="" className="border w-18 py-1 text-grey-61666D text-sm font-normal bg-gray-50 rounded-md tracking-wider">
+      <a href={local_website} className="border w-18 py-1 text-grey-61666D text-sm font-normal bg-gray-50 rounded-md tracking-wider">
         {props.name}
       </a>
     </Fragment>
--- src/ChannelLinkR.js
+++ src/ChannelLinkR.js
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
 
 function ChannelLinkR(props) {
+  const local_website = "http://localhost:3000/";
 
   return (
     <Fragment>
-      <a href="" className="px-2.5 py-1 text-grey-61666D text-sm font-normal">
+      <a href={local_website} className="px-2.5 py-1 text-grey-61666D text-sm font-normal">
         {props.name}
       </a>
     </Fragment>
